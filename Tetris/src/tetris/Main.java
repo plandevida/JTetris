beae3b209f053c6f0904f0824319d113def1cbb2
Dani <dserrano.torres@gmail.com>
2012-07-13 17:57:14 +0200

    minimal changes on Tetris class

--- Tetris/src/tetris/Main.java
+++ Tetris/src/tetris/Main.java
@@ -8,8 +8,9 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Tetris tetris = new Tetris();
+		Tetris tetris = new Tetris("JTetris");
 		tetris.setLayout(null);
+		tetris.setBounds(200, 200, 500, 700);
 		
 		tetris.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		tetris.setVisible(Boolean.TRUE);
