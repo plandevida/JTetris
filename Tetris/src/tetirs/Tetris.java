fda18b6085ccaf5bc92f4f7e526c4b454b44cd4a
Dani <dserrano.torres@gmail.com>
2012-07-13 13:24:53 +0200

    Tetris game on Java

---
+++ Tetris/src/tetirs/Tetris.java
@@ -0,0 +1,37 @@
+package tetirs;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class Tetris extends JFrame {
+
+	public Tetris() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Tetris(GraphicsConfiguration gc) {
+		super(gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Tetris(String title) throws HeadlessException {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Tetris(String title, GraphicsConfiguration gc) {
+		super(title, gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
